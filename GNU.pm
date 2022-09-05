ca9369f7459337bd20243a90289d840da65a6397
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-05 11:58:43 +0200

    Prepare for 0.04 version

--- GNU.pm
+++ GNU.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use Path::Tiny qw(path);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub exe {
 	my $self = shift;
@@ -98,6 +98,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
