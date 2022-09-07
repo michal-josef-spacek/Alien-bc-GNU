88a86d6aa80f395b6296a732ab31bd28e9375d9f
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-07 08:54:39 +0200

    Fix example output (s/~home/~/)

--- examples/print_variables.pl
+++ examples/print_variables.pl
@@ -12,12 +12,12 @@ print 'install_type: '.Alien::bc::GNU->install_type."\n";
 print 'version: '.Alien::bc::GNU->version."\n";
 
 # Output like (share):
-# bin_dir: ~home/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-bc-GNU/bin
-# dist_dir: ~home/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-bc-GNU
+# bin_dir: ~/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-bc-GNU/bin
+# dist_dir: ~/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-bc-GNU
 # install_type: share
 # version: 1.07.1
 
 # Output like (system):
-# dist_dir: ~home/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-bc-GNU
+# dist_dir: ~/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-bc-GNU
 # install_type: system
 # version: 1.07.1
